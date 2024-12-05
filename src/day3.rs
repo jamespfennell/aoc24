b9162b832a3004adbef861bfd5812906435de386
James Fennell <jamespfennell@gmail.com>
2024-12-05 10:02:55 -0500

    Day 3, problem 2

--- src/day3.rs
+++ src/day3.rs
@@ -1,63 +1,79 @@
 use std::fs;
 
 pub fn problem_1() -> i32 {
+    problem(false)
+}
+
+pub fn problem_2() -> i32 {
+    problem(true)
+}
+
+pub fn problem(do_and_dont: bool) -> i32 {
     let data = fs::read_to_string("data/day3.txt").expect("expected file to exist");
     let mut state: State = Default::default();
     let mut sum = 0;
-    for c in data.chars() {
-        let (new_state, pair) = state.consume(c);
-        if let Some((lhs, rhs)) = pair {
-            sum += lhs * rhs;
-        }
-        state = new_state
+    let mut enabled = true;
+    for next in data.chars() {
+        use State::*;
+        state = match (state, next) {
+            (FunctionName(s), '(') => {
+                if s.ends_with("do") {
+                    NoArg("do")
+                } else if s.ends_with("don't") {
+                    NoArg("don't")
+                } else if s.ends_with("mul") {
+                    FirstArg(0)
+                } else {
+                    Default::default()
+                }
+            }
+            (FunctionName(mut s), c) => {
+                s.push(c);
+                FunctionName(s)
+            }
+            (NoArg(fn_name), ')') => {
+                if fn_name == "do" {
+                    enabled = true;
+                }
+                if fn_name == "don't" && do_and_dont {
+                    enabled = false;
+                }
+                Default::default()
+            }
+            (FirstArg(current), '0'..='9') => {
+                let r: i32 = (next as usize - '0' as usize)
+                    .try_into()
+                    .expect("digit fits in i32");
+                FirstArg(current * 10 + r)
+            }
+            (FirstArg(current), ',') => SecondArg(current, 0),
+            (SecondArg(first, current), '0'..='9') => {
+                let r: i32 = (next as usize - '0' as usize)
+                    .try_into()
+                    .expect("digit fits in i32");
+                SecondArg(first, current * 10 + r)
+            }
+            (SecondArg(first, second), ')') => {
+                if enabled {
+                    sum += first * second;
+                }
+                Default::default()
+            }
+            (_, c) => FunctionName(c.into()),
+        };
     }
     sum
 }
 
-#[derive(Clone, Copy)]
 enum State {
-    ExpectPrefix(char),
-    ExpectFirstNumber(i32),
-    ExpectSecondNumber(i32, i32),
+    FunctionName(String),
+    NoArg(&'static str),
+    FirstArg(i32),
+    SecondArg(i32, i32),
 }
 
 impl Default for State {
     fn default() -> Self {
-        State::ExpectPrefix('m')
+        State::FunctionName(String::new())
     }
-}
-
-impl State {
-    fn consume(self, next: char) -> (Self, Option<(i32, i32)>) {
-        use State::*;
-        let n = match (self, next) {
-            (ExpectPrefix('m'), 'm') => ExpectPrefix('u'),
-            (ExpectPrefix('u'), 'u') => ExpectPrefix('l'),
-            (ExpectPrefix('l'), 'l') => ExpectPrefix('('),
-            (ExpectPrefix('('), '(') => ExpectFirstNumber(0),
-            // same for other digits, probably a fancy way to do this
-            (ExpectFirstNumber(current), '0'..='9') => {
-                let r: i32 = (next as usize - '0' as usize)
-                    .try_into()
-                    .expect("digit fits in i32");
-                ExpectFirstNumber(current * 10 + r)
-            }
-            (ExpectFirstNumber(current), ',') => ExpectSecondNumber(current, 0),
-            (ExpectSecondNumber(first, current), '0'..='9') => {
-                let r: i32 = (next as usize - '0' as usize)
-                    .try_into()
-                    .expect("digit fits in i32");
-                ExpectSecondNumber(first, current * 10 + r)
-            }
-            (ExpectSecondNumber(first, second), ')') => {
-                return (Default::default(), Some((first, second)));
-            }
-            _ => Default::default(),
-        };
-        (n, None)
-    }
-}
-
-pub fn problem_2() -> i32 {
-    0
 }
