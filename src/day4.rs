d08793ec76e051b12ae3775608840b1bd746e2ac
James Fennell <jamespfennell@gmail.com>
2024-12-05 18:22:15 -0500

    Day 4, problem 2

--- src/day4.rs
+++ src/day4.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{collections::HashSet, fs};
 
 const XMAS: [char; 4] = ['X', 'M', 'A', 'S'];
 const SAMX: [char; 4] = ['S', 'A', 'M', 'X'];
@@ -27,12 +27,7 @@ pub fn problem_1() -> i32 {
         };
         num_matches += iter.filter(|i| *i == XMAS || *i == SAMX).count();
     }
-    // 1651 <- too low
     num_matches.try_into().unwrap()
-}
-
-pub fn problem_2() -> i32 {
-    0
 }
 
 struct Iter<'a> {
@@ -90,9 +85,65 @@ impl Mode {
     }
 }
 
+pub fn problem_2() -> i32 {
+    let data = fs::read_to_string("data/day4.txt").expect("expected file to exist");
+    problem_2_with_input(&data)
+}
+
+pub fn problem_2_with_input(data: &str) -> i32 {
+    let lines: Vec<Vec<char>> = data
+        .lines()
+        .map(|l| {
+            let l: Vec<char> = l.chars().collect();
+            l
+        })
+        .collect();
+    let mut hits = 0;
+    let allowed_corners: HashSet<[[char; 2]; 2]> = [
+        [['M', 'M'], ['S', 'S']],
+        [['M', 'S'], ['M', 'S']],
+        [['S', 'M'], ['S', 'M']],
+        [['S', 'S'], ['M', 'M']],
+    ]
+    .into_iter()
+    .collect();
+    for r in 0..lines.len() - 2 {
+        for c in 0..lines[0].len() - 2 {
+            if lines[r + 1][c + 1] != 'A' {
+                continue;
+            }
+            let corners = [
+                [lines[r][c], lines[r][c + 2]],
+                [lines[r + 2][c], lines[r + 2][c + 2]],
+            ];
+            if allowed_corners.contains(&corners) {
+                hits += 1;
+            }
+        }
+    }
+    // 939 too low
+    // 1936 too high
+    hits
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+    #[test]
+    fn test_problem_2() {
+        let data = ".M.S......
+..A..MSMS.
+.M.S.MAA..
+..A.ASMSM.
+.M.S.M....
+..........
+S.S.S.S.S.
+.A.A.A.A..
+M.M.M.M.M.
+..........";
+        assert_eq!(9, problem_2_with_input(&data))
+    }
+
     #[test]
     fn test_iter_diagonal() {
         let input = vec![
@@ -117,6 +168,7 @@ mod tests {
         ];
         assert_eq!(got, want);
     }
+
     #[test]
     fn test_iter_horizontal() {
         let input = vec![
@@ -147,6 +199,7 @@ mod tests {
         ];
         assert_eq!(got, want);
     }
+
     #[test]
     fn test_iter_vertical() {
         let input = vec![
