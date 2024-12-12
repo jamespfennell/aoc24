5718ee9d5f78c06cfe209cc7ef9c08b377a40609
James Fennell <jamespfennell@gmail.com>
2024-12-12 18:19:53 -0500

    Day 12, problem 2

--- src/day10.rs
+++ src/day10.rs
@@ -1,3 +1,4 @@
+use super::iter::Neighbors;
 use std::collections::HashSet;
 
 pub fn problem_1(data: &str) -> i64 {
@@ -55,13 +56,7 @@ fn build_reachable(data: &str) -> (Vec<Vec<u32>>, Vec<Vec<Vec<(usize, usize)>>>)
                 if rows[r][c] != level {
                     continue;
                 }
-                let neighbors = Neighbors {
-                    max_r,
-                    max_c,
-                    r,
-                    c,
-                    next: 0,
-                };
+                let neighbors = Neighbors::new(&rows, r, c);
                 for (next_r, next_c) in neighbors {
                     if rows[next_r][next_c] != level + 1 {
                         continue;
@@ -76,48 +71,6 @@ fn build_reachable(data: &str) -> (Vec<Vec<u32>>, Vec<Vec<Vec<(usize, usize)>>>)
         }
     }
     (rows, reachable)
-}
-
-struct Neighbors {
-    max_r: usize,
-    max_c: usize,
-    c: usize,
-    r: usize,
-    next: usize,
-}
-
-impl Iterator for Neighbors {
-    type Item = (usize, usize);
-
-    fn next(&mut self) -> Option<Self::Item> {
-        loop {
-            let n = self.next;
-            self.next += 1;
-            match n {
-                0 => {
-                    if let Some(r) = self.r.checked_sub(1) {
-                        return Some((r, self.c));
-                    }
-                }
-                1 => {
-                    if let Some(c) = self.c.checked_sub(1) {
-                        return Some((self.r, c));
-                    }
-                }
-                2 => {
-                    if self.r + 1 < self.max_r {
-                        return Some((self.r + 1, self.c));
-                    }
-                }
-                3 => {
-                    if self.c + 1 < self.max_c {
-                        return Some((self.r, self.c + 1));
-                    }
-                }
-                _ => return None,
-            }
-        }
-    }
 }
 
 #[cfg(test)]
--- src/day12.rs
+++ src/day12.rs
@@ -23,8 +23,98 @@ pub fn problem_1(data: &str) -> i64 {
     sum
 }
 
-pub fn problem_2(_data: &str) -> i64 {
-    0
+pub fn problem_2(data: &str) -> i64 {
+    let grid: Vec<Vec<char>> = data.lines().map(|line| line.chars().collect()).collect();
+    let regions = build_regions(&grid);
+    let mut sum = 0_i64;
+    for region in &regions {
+        let mut perimeter = 0_i64;
+        let mut area = 0_i64;
+        for (r, c) in region.iter().copied() {
+            area += 1;
+            perimeter += 4;
+            let plant = grid[r][c];
+            for (r, c) in Neighbors::new(&grid, r, c) {
+                if grid[r][c] == plant {
+                    perimeter -= 1;
+                }
+            }
+        }
+
+        // Now we deduct shared continuous borders
+
+        // First we check if (r,c) shares a continuous verical border with (r+1,c)
+        // on either side for each (r,c) in the region.
+        for (r, c) in region.iter().copied() {
+            // If we're on the bottom of the grid, skip becuase there is no square
+            // below to share a border with.
+            if r + 1 == grid.len() {
+                continue;
+            }
+            // If the box below is different, skip
+            if grid[r + 1][c] != grid[r][c] {
+                continue;
+            }
+            // First check the left hand side. If there is a continuous vertical
+            // border then the two squares to the left have a different plant
+            // (or no plant if on the grid edge).
+            let different_left = match c.checked_sub(1) {
+                None => true,
+                Some(c_minus_1) => {
+                    grid[r][c_minus_1] != grid[r][c] && grid[r + 1][c_minus_1] != grid[r + 1][c]
+                }
+            };
+            if different_left {
+                perimeter -= 1;
+            }
+            // Same for the right
+            let different_right = if c + 1 == grid[0].len() {
+                true
+            } else {
+                grid[r][c + 1] != grid[r][c] && grid[r + 1][c + 1] != grid[r + 1][c]
+            };
+            if different_right {
+                perimeter -= 1;
+            }
+        }
+
+        // Next (r,c) and (r+1,c)
+        for (r, c) in region.iter().copied() {
+            // If we're on the right side of grid, skip becuase there is no square
+            // right to share a border with.
+            if c + 1 == grid.len() {
+                continue;
+            }
+            // If the box to the right is different, skip
+            if grid[r][c + 1] != grid[r][c] {
+                continue;
+            }
+            // First check above. If there is a continuous vertical
+            // border then the two squares above have a different plant
+            // (or no plant if on the grid edge).
+            let different_above = match r.checked_sub(1) {
+                None => true,
+                Some(r_minus_1) => {
+                    grid[r_minus_1][c] != grid[r][c] && grid[r_minus_1][c + 1] != grid[r][c + 1]
+                }
+            };
+            if different_above {
+                perimeter -= 1;
+            }
+            // Same for the bottom
+            let different_bottom = if r + 1 == grid.len() {
+                true
+            } else {
+                grid[r + 1][c] != grid[r][c] && grid[r + 1][c + 1] != grid[r][c + 1]
+            };
+            if different_bottom {
+                perimeter -= 1;
+            }
+        }
+
+        sum += area * perimeter;
+    }
+    sum
 }
 
 fn build_regions(grid: &[Vec<char>]) -> Vec<Vec<(usize, usize)>> {
@@ -77,6 +167,19 @@ MIIIIIJJEE
 MIIISIJEEE
 MMMISSJEEE";
 
+    const DATA_3: &str = "EEEEE
+EXXXX
+EEEEE
+EXXXX
+EEEEE";
+
+    const DATA_4: &str = "AAAAAA
+AAABBA
+AAABBA
+ABBAAA
+ABBAAA
+AAAAAA";
+
     #[test]
     fn test_problem_1_data_1() {
         assert_eq!(140, problem_1(DATA_1));
@@ -88,7 +191,22 @@ MMMISSJEEE";
     }
 
     #[test]
-    fn test_problem_2() {
-        assert_eq!(0, problem_2(DATA_1));
+    fn test_problem_2_data_1() {
+        assert_eq!(80, problem_2(DATA_1));
+    }
+
+    #[test]
+    fn test_problem_2_data_2() {
+        assert_eq!(1206, problem_2(DATA_2));
+    }
+
+    #[test]
+    fn test_problem_2_data_3() {
+        assert_eq!(236, problem_2(DATA_3));
+    }
+
+    #[test]
+    fn test_problem_2_data_4() {
+        assert_eq!(368, problem_2(DATA_4));
     }
 }
