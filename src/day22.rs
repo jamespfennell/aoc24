74f9e1e88ef1880bf8a3a2ae9e8d8a00e1ce4ce2
James Fennell <jamespfennell@gmail.com>
2024-12-22 11:32:20 -0500

    Day 22, problem 2

--- src/day22.rs
+++ src/day22.rs
@@ -1,3 +1,5 @@
+use std::{collections::HashMap, i64};
+
 pub fn problem_1(data: &str) -> i64 {
     data.lines()
         .map(|l| {
@@ -10,6 +12,43 @@ pub fn problem_1(data: &str) -> i64 {
         .sum()
 }
 
+pub fn problem_2(data: &str) -> i64 {
+    let maps: Vec<HashMap<[i8; 4], i64>> = data
+        .lines()
+        .map(|l| {
+            let mut n: i64 = l.parse().unwrap();
+            let mut diffs: Vec<i8> = vec![];
+            let mut prices: Vec<i64> = vec![];
+            for _ in 0..2000 {
+                let next_n = evolve_secret(n);
+                prices.push(next_n % 10);
+                diffs.push((next_n % 10 - n % 10).try_into().unwrap());
+                n = next_n;
+            }
+
+            let mut m: HashMap<[i8; 4], i64> = Default::default();
+            for i in 3..2000 {
+                let key = [diffs[i - 3], diffs[i - 2], diffs[i - 1], diffs[i]];
+                m.entry(key).or_insert(prices[i]);
+            }
+            m
+        })
+        .collect();
+
+    let mut merged: HashMap<[i8; 4], i64> = Default::default();
+    for m in maps {
+        for (k, v) in m {
+            *merged.entry(k).or_default() += v;
+        }
+    }
+
+    let mut max = 0;
+    for (_, v) in merged {
+        max = max.max(v);
+    }
+    max
+}
+
 fn evolve_secret(n: i64) -> i64 {
     let n = ((n * 64) ^ n) % 16777216;
     let n = ((n / 32) ^ n) % 16777216;
@@ -17,27 +56,17 @@ fn evolve_secret(n: i64) -> i64 {
     n
 }
 
-/*
-Calculate the result of multiplying the secret number by 64. Then, mix this result into the secret number. Finally, prune the secret number.
-Calculate the result of dividing the secret number by 32. Round the result down to the nearest integer. Then, mix this result into the secret number. Finally, prune the secret number.
-Calculate the result of multiplying the secret number by 2048. Then, mix this result into the secret number. Finally, prune the secret number.
-
-Each step of the above process involves mixing and pruning:
-
-To mix a value into the secret number, calculate the bitwise XOR of the given value and the secret number. Then, the secret number becomes the result of that operation. (If the secret number is 42 and you were to mix 15 into the secret number, the secret number would become 37.)
-To prune the secret number, calculate the value of the secret number modulo 16777216. Then, the secret number becomes the result of that operation. (If the secret number is 100000000 and you were to prune the secret number, the secret number would become 16113920.)
- */
-
-pub fn problem_2(_data: &str) -> i64 {
-    0
-}
-
 #[cfg(test)]
 mod test {
     use super::*;
-    const DATA: &str = "1
+    const DATA_1: &str = "1
 10
 100
+2024";
+
+    const DATA_2: &str = "1
+2
+3
 2024";
 
     #[test]
@@ -52,6 +81,7 @@ mod test {
 12249484
 7753432
 5908254";
+
         let secrets: Vec<i64> = secrets.lines().map(|l| l.parse().unwrap()).collect();
         let mut n = 123;
         for secret in secrets {
@@ -62,7 +92,7 @@ mod test {
     }
 
     super::super::tests::tests!(
-        (test_problem_1_data_1, problem_1, DATA, 37327623),
-        (test_problem_2_data_1, problem_2, DATA, 0),
+        (test_problem_1_data_1, problem_1, DATA_1, 37327623),
+        (test_problem_2_data_2, problem_2, DATA_2, 23),
     );
 }
